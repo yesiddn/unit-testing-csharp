b32b65926bd6d955b8f778e96a04944427e923d0
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-12 23:12:04 -0500

    Add my first .NET test

---
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -0,0 +1,14 @@
+﻿namespace StringManipulation.Tests;
+
+public class StringOperationsTest
+{
+    [Fact]
+    public void ConcatenateStrings()
+    {
+        var strOperations = new StringOperations();
+
+        var result = strOperations.ConcatenateStrings("Hello", "world");
+
+        Assert.Equal("Hello world", result);
+    }
+}
---
+++ StringManipulation.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace StringManipulation.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
