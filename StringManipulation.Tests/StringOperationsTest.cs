75b3694cc7a870da7ad01473f822d416fb749879
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-13 21:53:26 -0500

    Add common Assert methods (part 2)

--- StringManipulation.Tests/StringOperationsTest.cs
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -44,4 +44,44 @@ public class StringOperationsTest
         // Assert
         Assert.False(result);
     }
+
+    [Fact]
+    public void QuantityInWords()
+    {
+        // Arrange
+        var strOperations = new StringOperations();
+
+        // Act
+        var result = strOperations.QuantintyInWords("cat", 10);
+
+        // Assert
+        Assert.StartsWith("ten", result);
+        Assert.Contains("cat", result);
+    }
+
+    [Fact]
+    public void GetStringLength_Exception()
+    {
+        // Arrange
+        var strOperations = new StringOperations();
+
+        // En este caso tenemos que omitir el Act porque va a saltar una excepcion
+
+        // Assert
+        // este metodo se le pasa el tipo de excepcion y como argumento una funcion lambda la cual le retornara al metodo la excepcion que se genera
+        Assert.ThrowsAny<ArgumentNullException>(() => strOperations.GetStringLength(null));
+    }
+
+    [Fact]
+    public void GetStringLength()
+    {
+        // Arrange
+        var strOperations = new StringOperations();
+
+        // Act
+        var result = strOperations.GetStringLength("world");
+
+        // Assert
+        Assert.Equal(5, result);
+    }
 }
