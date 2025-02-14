ad9997cef597ed8a0e688cf596912243473fa80b
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-13 21:26:16 -0500

    Add common Assert methods

--- StringManipulation.Tests/StringOperationsTest.cs
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -5,10 +5,43 @@ public class StringOperationsTest
     [Fact]
     public void ConcatenateStrings()
     {
+        // Arrange
         var strOperations = new StringOperations();
 
+        // Act
         var result = strOperations.ConcatenateStrings("Hello", "world");
 
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotEmpty(result);
         Assert.Equal("Hello world", result);
+    }
+
+    // es buena practica con los booleanos que existan pruebas diferentes para ambos casos, true y false
+    // para diferenciar los metodos, en los nombres se usa _False o _True
+    [Fact]
+    public void IsPalindrome_True()
+    {
+        // Arrangevar
+        var strOperations = new StringOperations();
+
+        // Act
+        var result = strOperations.IsPalindrome("ama");
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsPalindrome_False()
+    {
+        // Arrangevar
+        var strOperations = new StringOperations();
+
+        // Act
+        var result = strOperations.IsPalindrome("roma");
+
+        // Assert
+        Assert.False(result);
     }
 }
